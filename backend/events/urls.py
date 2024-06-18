da821371dc8534aa66cf63e33a610ab92afb7535
sfuent1 <sfuent1@gmail.com>
2024-06-18 17:17:35 -0400

    initial backend setup

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,11 +1,1 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import EventViewSet, GuestViewSet
-
-router = DefaultRouter()
-router.register(r'events', EventViewSet)
-router.register(r'guests', GuestViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]

af768c001a7354ebbdd6bb408fe076561b7622af
sfuent1 <sfuent1@gmail.com>
2024-06-21 12:19:47 -0400

    initial backend setup w/ CRUD

--- backend/events/views.py
+++ backend/events/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import get_object_or_404
 from .models import Event
 from .serializers import EventSerializer
 
-
 class EventList(APIView):
     def get(self, request):
         events = Event.objects.all()
@@ -15,22 +14,23 @@ class EventList(APIView):
         serializer = EventSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             event = serializer.save()
-            return Response({"result": f"Event id {event.id} created"})
+            return Response({"result": f"Event {event.id} created"}, status=201)
 
-    def put(self, request, pk):
-        event = get_object_or_404(Event.objects.all(), pk=pk)
-        serializer = EventSerializer(instance=event, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            event = serializer.save()
-            return Response({"result": f"Event id {event.id} updated"})
-
-    def delete(self, request, pk):
-        event = get_object_or_404(Event.objects.all(), pk=pk)
-        event.delete()
-        return Response({"result": f"Event id {pk} deleted"}, status=204)
 
 class EventDetail(APIView):
     def get(self, request, pk):
         event = get_object_or_404(Event.objects.all(), pk=pk)
         serializer = EventSerializer(event)
         return Response({"result": serializer.data})
+    
+    def put(self, request, pk):
+        event = get_object_or_404(Event.objects.all(), pk=pk)
+        serializer = EventSerializer(instance=event, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            event = serializer.save()
+            return Response({"result": f"Event {event.id} updated"})
+
+    def delete(self, request, pk):
+        event = get_object_or_404(Event.objects.all(), pk=pk)
+        event.delete()
+        return Response({"result": f"Event {pk} deleted"}, status=204)
--- backend/guests/views.py
+++ backend/guests/views.py
@@ -4,33 +4,33 @@ from django.shortcuts import get_object_or_404
 from .models import Guest
 from .serializers import GuestSerializer
 
-
 class GuestList(APIView):
     def get(self, request):
         guests = Guest.objects.all()
         serializer = GuestSerializer(guests, many=True)
         return Response({"result": serializer.data})
-
+    
     def post(self, request):
         serializer = GuestSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             guest = serializer.save()
-            return Response({"result": f"Guest id {guest.id} created"})
-
-    def put(self, request, pk):
-        guest = get_object_or_404(Guest.objects.all(), pk=pk)
-        serializer = GuestSerializer(instance=guest, data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            guest = serializer.save()
-            return Response({"result": f"Guest id {guest.id} updated"})
-
-    def delete(self, request, pk):
-        guest = get_object_or_404(Guest.objects.all(), pk=pk)
-        guest.delete()
-        return Response({"result": f"Guest id {pk} deleted"}, status=204)
+            return Response({"result": f"Guest {guest.id} created"}, status=201)
+    
 
 class GuestDetail(APIView):
     def get(self, request, pk):
         guest = get_object_or_404(Guest.objects.all(), pk=pk)
         serializer = GuestSerializer(guest)
         return Response({"result": serializer.data})
+    
+    def put(self, request, pk):
+        guest = get_object_or_404(Guest.objects.all(), pk=pk)
+        serializer = GuestSerializer(instance=guest, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            guest = serializer.save()
+            return Response({"result": f"Guest {guest.id} updated"})
+    
+    def delete(self, request, pk):
+        guest = get_object_or_404(Guest.objects.all(), pk=pk)
+        guest.delete()
+        return Response({"result": f"Guest {pk} deleted"}, status=204)

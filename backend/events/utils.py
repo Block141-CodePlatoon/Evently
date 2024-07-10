3ed51b572bb189a2797e08b9add949b47e3fe91d
emcke1 <ekmckenzie26@gmail.com>
2024-07-09 20:21:13 -0400

    google-api-changes

--- backend/events/utils.py
+++ backend/events/utils.py
@@ -1,8 +1,10 @@
 # events/utils.py
 
 import os
+import googlemaps
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from django.conf import settings
 
 def send_email(to_email, subject, content, from_email):
     sg = SendGridAPIClient(api_key=os.getenv('SENDGRID_API_KEY'))
@@ -14,3 +16,13 @@ def send_email(to_email, subject, content, from_email):
     )
     response = sg.send(message)
     return response.status_code
+
+
+def get_directions(origin, destination):
+    gmaps = googlemaps.Client(key=settings.GOOGLE_MAPS_API_KEY)
+    try:
+        directions = gmaps.directions(origin, destination)
+        return directions
+    except Exception as e:
+        print(f"Failed to retrieve directions: {e}")
+        return None

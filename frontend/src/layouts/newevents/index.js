3f17f0a7f027758434cd8c3c9dba6e7af4198b51
Block141 <block141@gmail.com>
2024-07-09 20:49:23 -0500

    Test psuh

--- frontend/src/layouts/newevents/index.js
+++ frontend/src/layouts/newevents/index.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
-import NewEventLayoutt from 'components/NewEventLayoutLayout';
+import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import NewEventLayout from 'components/NewEventLayout';
 import NewEventNavbar from 'components/NewEventNavbar';
 import Footer from 'components/Footer';
+import axios from '../../axiosSetup'; // Ensure axios is imported from axiosSetup.js
 
-function NewEventLayout({ children }) {
+function NewEventLayout2({ children }) {
+  const { id } = useParams();
+  const [eventName, setEventName] = useState('');
+
+  useEffect(() => {
+    const fetchEventData = async () => {
+      try {
+        console.log(`Fetching event data for event ID: ${id}`);
+        const response = await axios.get(`/events/${id}/`);
+        console.log('Fetched event data:', response.data);
+        setEventName(response.data.title);
+      } catch (error) {
+        console.error('Error fetching event data:', error);
+      }
+    };
+
+    if (id) {
+      fetchEventData();
+    }
+  }, [id]);
+
   return (
-    <DashboardLayout>
-      <NewEventNavbar />
+    <NewEventLayout>
+      <NewEventNavbar eventName={eventName} /> {/* Pass eventName as a prop */}
       <Box p={3}>
-        {children}
+        {React.cloneElement(children, { eventName })} {/* Pass eventName as a prop to children */}
       </Box>
       <Footer />
-    </DashboardLayout>
+    </NewEventLayout>
   );
 }
 
-export default NewEventLayout;
+NewEventLayout2.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default NewEventLayout2;
